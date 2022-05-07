4b35e7cf63eafa7943a1fe6cdc0d23ed0c80d8dd
hamza nassour <hamza.nassour@edu.uca.ma>
2022-05-07 12:44:22 -0700

    err

---
+++ src/main/java/com/ensapay/ebanking/repositories/AgentRepository.java
@@ -0,0 +1,10 @@
+package com.ensapay.ebanking.repositories;
+
+import com.ensapay.ebanking.entities.Agent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AgentRepository extends JpaRepository<Agent , Long> {
+
+
+
+}
